1716b13e8b2841613c10b1b8b9494a559015ba92
SariSafe <98646866+SariSafe@users.noreply.github.com>
2022-07-14 09:32:42 +0300

    mistake !just one " ! " the network is interrupted

--- EchoClient/src/Program/Main/Main.java
+++ EchoClient/src/Program/Main/Main.java
@@ -30,7 +30,7 @@ public class Main {
                      System.out.println(response);
                  }
              }
-             while (!!echoString.equals("exit"));
+             while (!echoString.equals("exit"));
          }
          catch (SocketTimeoutException e)
          {
